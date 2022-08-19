fa26796f069019cc0928b6c38cda2eeda84d41da
iosdevIslombek <islomjonrazzoqov@gmail.com>
2022-08-19 17:19:08 +0500

    Initial Commit

---
+++ Avito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito
+//
+//  Created by Islomjon on 19/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
